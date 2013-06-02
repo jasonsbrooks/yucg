1434183c2d7310016b5286f034a2b8add0d29459
jasonsbrooks <jason.brooks@yale.edu>
2013-06-01 23:14:47 -0400

    Fixed scrolling divs and project porfolio boxes

---
+++ public/js/yucg.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+  function goToByScroll(id){
+    // Reove "link" from the ID
+    id = id.replace("link", "");
+      // Scroll
+    $('html,body').animate({
+        scrollTop: $("#div-"+id).offset().top-55},
+        'slow');
+  }
+
+  $("#mainmenu > ul > li > a").click(function(e) { 
+        // Prevent a page reload when a link is pressed
+      e.preventDefault(); 
+        // Call the scroll function
+      goToByScroll($(this).attr("id"));           
+  });
+});
